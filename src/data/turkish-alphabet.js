b04f9951ce4562939fb6b9b4f8b992480b8af9be
Mammad Oznur <Mammadoznur@Gmail.com>
2026-09-17 08:56:44 +0300

    Add Turkish alphabet learning data

---
+++ src/data/turkish-alphabet.js
@@ -0,0 +1,292 @@
+window.MONTESSORI_TURKISH_ALPHABET = Object.freeze([
+  {
+    id: 'a',
+    letter: 'A',
+    lowercase: 'a',
+    name: 'a',
+    phoneme: 'a',
+    phonemeType: 'direct',
+    example: 'Arı',
+    exampleEmoji: '🐝'
+  },
+  {
+    id: 'b',
+    letter: 'B',
+    lowercase: 'b',
+    name: 'be',
+    phoneme: 'b',
+    phonemeType: 'direct',
+    example: 'Balık',
+    exampleEmoji: '🐟'
+  },
+  {
+    id: 'c',
+    letter: 'C',
+    lowercase: 'c',
+    name: 'ce',
+    phoneme: 'c',
+    phonemeType: 'direct',
+    example: 'Ceviz',
+    exampleEmoji: '🌰'
+  },
+  {
+    id: 'ç',
+    letter: 'Ç',
+    lowercase: 'ç',
+    name: 'çe',
+    phoneme: 'ç',
+    phonemeType: 'direct',
+    example: 'Çiçek',
+    exampleEmoji: '🌸'
+  },
+  {
+    id: 'd',
+    letter: 'D',
+    lowercase: 'd',
+    name: 'de',
+    phoneme: 'd',
+    phonemeType: 'direct',
+    example: 'Deniz',
+    exampleEmoji: '🌊'
+  },
+  {
+    id: 'e',
+    letter: 'E',
+    lowercase: 'e',
+    name: 'e',
+    phoneme: 'e',
+    phonemeType: 'direct',
+    example: 'Elma',
+    exampleEmoji: '🍎'
+  },
+  {
+    id: 'f',
+    letter: 'F',
+    lowercase: 'f',
+    name: 'fe',
+    phoneme: 'f',
+    phonemeType: 'direct',
+    example: 'Fil',
+    exampleEmoji: '🐘'
+  },
+  {
+    id: 'g',
+    letter: 'G',
+    lowercase: 'g',
+    name: 'ge',
+    phoneme: 'g',
+    phonemeType: 'direct',
+    example: 'Güneş',
+    exampleEmoji: '☀️'
+  },
+  {
+    id: 'ğ',
+    letter: 'Ğ',
+    lowercase: 'ğ',
+    name: 'yumuşak ge',
+    phoneme: null,
+    phonemeType: 'context-dependent',
+    example: 'Dağ',
+    exampleEmoji: '🏔️'
+  },
+  {
+    id: 'h',
+    letter: 'H',
+    lowercase: 'h',
+    name: 'he',
+    phoneme: 'h',
+    phonemeType: 'direct',
+    example: 'Havuç',
+    exampleEmoji: '🥕'
+  },
+  {
+    id: 'ı',
+    letter: 'I',
+    lowercase: 'ı',
+    name: 'ı',
+    phoneme: 'ı',
+    phonemeType: 'direct',
+    example: 'Işık',
+    exampleEmoji: '💡'
+  },
+  {
+    id: 'i',
+    letter: 'İ',
+    lowercase: 'i',
+    name: 'i',
+    phoneme: 'i',
+    phonemeType: 'direct',
+    example: 'İnek',
+    exampleEmoji: '🐄'
+  },
+  {
+    id: 'j',
+    letter: 'J',
+    lowercase: 'j',
+    name: 'je',
+    phoneme: 'j',
+    phonemeType: 'direct',
+    example: 'Jel',
+    exampleEmoji: '🫧'
+  },
+  {
+    id: 'k',
+    letter: 'K',
+    lowercase: 'k',
+    name: 'ke',
+    phoneme: 'k',
+    phonemeType: 'direct',
+    example: 'Kedi',
+    exampleEmoji: '🐈'
+  },
+  {
+    id: 'l',
+    letter: 'L',
+    lowercase: 'l',
+    name: 'le',
+    phoneme: 'l',
+    phonemeType: 'direct',
+    example: 'Limon',
+    exampleEmoji: '🍋'
+  },
+  {
+    id: 'm',
+    letter: 'M',
+    lowercase: 'm',
+    name: 'me',
+    phoneme: 'm',
+    phonemeType: 'direct',
+    example: 'Masa',
+    exampleEmoji: '🪑'
+  },
+  {
+    id: 'n',
+    letter: 'N',
+    lowercase: 'n',
+    name: 'ne',
+    phoneme: 'n',
+    phonemeType: 'direct',
+    example: 'Nar',
+    exampleEmoji: '🍎'
+  },
+  {
+    id: 'o',
+    letter: 'O',
+    lowercase: 'o',
+    name: 'o',
+    phoneme: 'o',
+    phonemeType: 'direct',
+    example: 'Orman',
+    exampleEmoji: '🌲'
+  },
+  {
+    id: 'ö',
+    letter: 'Ö',
+    lowercase: 'ö',
+    name: 'ö',
+    phoneme: 'ö',
+    phonemeType: 'direct',
+    example: 'Ördek',
+    exampleEmoji: '🦆'
+  },
+  {
+    id: 'p',
+    letter: 'P',
+    lowercase: 'p',
+    name: 'pe',
+    phoneme: 'p',
+    phonemeType: 'direct',
+    example: 'Panda',
+    exampleEmoji: '🐼'
+  },
+  {
+    id: 'r',
+    letter: 'R',
+    lowercase: 'r',
+    name: 're',
+    phoneme: 'r',
+    phonemeType: 'direct',
+    example: 'Renk',
+    exampleEmoji: '🌈'
+  },
+  {
+    id: 's',
+    letter: 'S',
+    lowercase: 's',
+    name: 'se',
+    phoneme: 's',
+    phonemeType: 'direct',
+    example: 'Su',
+    exampleEmoji: '💧'
+  },
+  {
+    id: 'ş',
+    letter: 'Ş',
+    lowercase: 'ş',
+    name: 'şe',
+    phoneme: 'ş',
+    phonemeType: 'direct',
+    example: 'Şemsiye',
+    exampleEmoji: '☂️'
+  },
+  {
+    id: 't',
+    letter: 'T',
+    lowercase: 't',
+    name: 'te',
+    phoneme: 't',
+    phonemeType: 'direct',
+    example: 'Tavşan',
+    exampleEmoji: '🐇'
+  },
+  {
+    id: 'u',
+    letter: 'U',
+    lowercase: 'u',
+    name: 'u',
+    phoneme: 'u',
+    phonemeType: 'direct',
+    example: 'Uçurtma',
+    exampleEmoji: '🪁'
+  },
+  {
+    id: 'ü',
+    letter: 'Ü',
+    lowercase: 'ü',
+    name: 'ü',
+    phoneme: 'ü',
+    phonemeType: 'direct',
+    example: 'Üzüm',
+    exampleEmoji: '🍇'
+  },
+  {
+    id: 'v',
+    letter: 'V',
+    lowercase: 'v',
+    name: 've',
+    phoneme: 'v',
+    phonemeType: 'direct',
+    example: 'Vazo',
+    exampleEmoji: '🏺'
+  },
+  {
+    id: 'y',
+    letter: 'Y',
+    lowercase: 'y',
+    name: 'ye',
+    phoneme: 'y',
+    phonemeType: 'direct',
+    example: 'Yaprak',
+    exampleEmoji: '🍃'
+  },
+  {
+    id: 'z',
+    letter: 'Z',
+    lowercase: 'z',
+    name: 'ze',
+    phoneme: 'z',
+    phonemeType: 'direct',
+    example: 'Zürafa',
+    exampleEmoji: '🦒'
+  }
+]);
